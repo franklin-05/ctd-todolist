bdbc7da30f62d6fdba833e0d3687dd2af439805b
Franklin Salazar <franklin@MacBookPro.lan>
2025-07-23 21:17:48 -0400

    added form, with button

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import './App.css'
+import './App.css';
+import TodoForm from './TodoForm.jsx'
+import TodoList from './TodoList.jsx'; // Importing TodoList
 
 function App() {
-  const todos=[
-    {id:1, title:"review resources"},
-    {id:2, title:"take notes"},
-    {id:3, title:"code out app"},
-  ]
-  return(
-    <div>
-      <h1>
-        My Todos
-      </h1>
-      <ul>
-        {todos.map(todos=> <li key={todos.id}>{todos.title} </li>)}
-      </ul>
-    </div>
-  )
-
-  
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <TodoForm />
+            <TodoList /> {/* Using the TodoList component here */}
+            
+        </div>
+    );
 }
 
-export default App
+export default App;
+
---
+++ src/TodoForm.jsx
@@ -0,0 +1,8 @@
+function TodoForm(){
+    return (<form>
+    <label for='todoTitle'> Todo</label>
+    <input type="text"></input>
+    <button>Add Todo</button>
+    </form>)
+}
+export default TodoForm
